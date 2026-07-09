aa26c803072de3c542d41ff4eeefa762180e2277
Khaoula <khaoula.mouanniss@gmail.com>
2026-07-08 21:21:49 -0400

    add comments

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ const closeVideoButton = document.getElementById("close-video");
 const videoPopup = document.getElementById("video-popup");
 const aquaequaVideo = document.getElementById("aquaequa-video");
 
+/* Ouvrir la popup vidéo */
 openVideoButton.addEventListener("click", function () {
     videoPopup.classList.add("active");
 });
 
+/* Fermer la popup avec le bouton */
 closeVideoButton.addEventListener("click", function () {
     videoPopup.classList.remove("active");
     aquaequaVideo.pause();
 });
 
+/* Fermer la popup en cliquant sur le fond sombre */
 videoPopup.addEventListener("click", function (event) {
     if (event.target === videoPopup) {
         videoPopup.classList.remove("active");
